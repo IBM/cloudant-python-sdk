c0524c63f0dcc3b4c38adb21eb2818b4c6cb72bb
Bessenyei Balázs Donát <bessbd@gmail.com>
2021-01-08 17:36:45 +0100

    Update copyright years

--- pylint/checkers/license-checker.py
+++ pylint/checkers/license-checker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# © Copyright IBM Corporation 2020.
+# © Copyright IBM Corporation 2021.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
