eaabca3e41f0b972ad7293aca3a19e462cef95c8
cloudant-sdks-automation <tblench@uk.ibm.com>
2021-01-13 18:33:23 +0000

    Update generated source code
    Generated SDK source code using:
    - Generator version 3.20.0
    - Specification version 0.0.29
    - Automation (cloudant-sdks) version 09816a0

--- ibmcloudant/cloudant_v1.py
+++ ibmcloudant/cloudant_v1.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# IBM OpenAPI SDK Code Generator Version: 3.21.0-a45d89ef-20201207-220514
+# IBM OpenAPI SDK Code Generator Version: 3.20.0-debb9f29-20201203-202043
  
 """
 NoSQL database based on Apache CouchDB
