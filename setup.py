9da567ae0092c6dbbd8bcde994aedc67135cf65c
Esteban Laver <emlaver@us.ibm.com>
2021-02-10 12:35:23 -0500

    Drop support for Python 3.5, add Python 3.9 (#75)

--- setup.py
+++ setup.py
@@ -79,10 +79,10 @@ setup(name=PACKAGE_NAME.replace('_', '-'),
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9'
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License',
