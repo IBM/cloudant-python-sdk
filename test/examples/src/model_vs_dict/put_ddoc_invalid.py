8f5317ba5ce82828f38c346a8b5fca422c03994b
vmatyusGitHub <veronika.matyus@gmail.com>
2022-03-16 18:40:12 +0100

    Add PUT `/{db}/{ddoc}` examples that uses model class XOR dictionaries (#223)
    * Introduce `model_vs_dict` folder for the new files
    * Add PUT `/{db}/{ddoc}` example files

---
+++ test/examples/src/model_vs_dict/put_ddoc_invalid.py
@@ -0,0 +1,20 @@
+from ibmcloudant.cloudant_v1 import CloudantV1, DesignDocument
+
+client = CloudantV1.new_instance()
+
+price_index = {
+    'index': 'function (doc) { index("price", doc.price); }',
+}
+
+partitioned_design_doc = DesignDocument(
+    indexes={'findByPrice': price_index},
+    options={'partitioned': True}
+)
+
+response = client.put_design_document(
+    db='products',
+    design_document=partitioned_design_doc,
+    ddoc='appliances'
+).get_result()
+
+print(response)
