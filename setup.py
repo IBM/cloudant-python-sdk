b85fab9b34320861441f75b5253e41a5b936d0be
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2022-03-28 09:47:14 +0000

    Update version 0.0.43 -> 0.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '0.0.43'
+__version__ = '0.1.0'
 PACKAGE_NAME = 'ibmcloudant'
 PACKAGE_DESC = 'Python client library for IBM Cloudant'
 
