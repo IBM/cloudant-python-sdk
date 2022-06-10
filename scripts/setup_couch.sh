e714aae02062b7d2e689b5ffcd3cbd11d2d66f37
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2022-06-10 14:28:45 +0100

    Update generated source code (#283)
    Generated SDK source code using:
    - Generator version 3.49.0
    - Specification version 1.0.0-dev0.0.44
    - Automation (cloudant-sdks) version a557afe

--- scripts/setup_couch.sh
+++ scripts/setup_couch.sh
@@ -4,8 +4,9 @@ set -ev
 
 printenv | grep "^SERVER_" > cloudant.env
 # if you change the image version please regenerate example output captures
-docker run -e COUCHDB_USER="$SERVER_USERNAME" -e COUCHDB_PASSWORD="$SERVER_PASSWORD" -p 5984:5984 -d apache/couchdb:3
+docker start couchdb || docker run --name couchdb --rm -e COUCHDB_USER="$SERVER_USERNAME" -e COUCHDB_PASSWORD="$SERVER_PASSWORD" -p 5984:5984 -d apache/couchdb:3
 # shellcheck disable=SC2016
 timeout 120 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ${SERVER_URL}/_up)" != "200" ]]; do sleep 5; done' || false
+curl -XPUT -u "$SERVER_USERNAME":"$SERVER_PASSWORD" "$SERVER_URL"/_users
 curl -XPUT -u "$SERVER_USERNAME":"$SERVER_PASSWORD" "$SERVER_URL"/stores
 curl -XPOST -u "$SERVER_USERNAME":"$SERVER_PASSWORD" -H "Content-type: application/json" -d '{}' "$SERVER_URL"/stores
