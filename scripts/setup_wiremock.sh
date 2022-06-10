8bff585006b08c4aedb0baeef57c4272c6430e13
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2022-06-10 13:02:54 +0000

    Update generated source code
    Generated SDK source code using:
    - Generator version 3.49.0
    - Specification version 1.0.0-dev0.0.44
    - Automation (cloudant-sdks) version a557afe

--- scripts/setup_wiremock.sh
+++ scripts/setup_wiremock.sh
@@ -4,7 +4,7 @@ set -ev
 
 printenv | grep "^WIREMOCK_" > wiremock.env
 
-docker run --rm -d -p "$WIREMOCK_PORT":8080 wiremock/wiremock:latest
+docker start wiremock || docker run --name wiremock --rm -d -p "$WIREMOCK_PORT":8080 wiremock/wiremock:latest
 timeout 120 bash -c 'while [[ "$(curl -s --location -o /dev/null -w ''%{http_code}'' ${WIREMOCK_URL}/__admin)" != "200" ]]; do sleep 2; done' || false
 curl "$WIREMOCK_URL"/__admin/mappings/import -X POST -d @stubs/mappings.json
 echo "Wiremock started"
