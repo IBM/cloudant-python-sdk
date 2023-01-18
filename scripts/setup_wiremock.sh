5be36acea64f28084911418f818636aadf5dd3c6
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2023-01-18 10:01:52 +0000

    chore(generated): pull QA images from artifactory, if env var set
    Generated SDK source code using:
    - Generator version 3.64.0
    - Specification version 1.0.0-dev0.0.49
    - Automation (cloudant-sdks) version 8917a5c
    
    Signed-off-by: cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>

--- scripts/setup_wiremock.sh
+++ scripts/setup_wiremock.sh
@@ -4,7 +4,8 @@ set -ev
 
 printenv | grep "^WIREMOCK_" > wiremock.env
 
-docker start wiremock || docker run --name wiremock --rm -d -p "$WIREMOCK_PORT":8080 wiremock/wiremock:latest
+# use image at ARTIFACTORY_DOCKER_REPO_VIRTUAL registry if set, otherwise use default registry
+docker start wiremock || docker run --name wiremock --rm -d -p "$WIREMOCK_PORT":8080 ${ARTIFACTORY_DOCKER_REPO_VIRTUAL:+${ARTIFACTORY_DOCKER_REPO_VIRTUAL}/}wiremock/wiremock:latest
 timeout 120 bash -c 'while [[ "$(curl -s --location -o /dev/null -w ''%{http_code}'' ${WIREMOCK_URL}/__admin)" != "200" ]]; do sleep 2; done' || false
 curl "$WIREMOCK_URL"/__admin/mappings/import -X POST -d @stubs/mappings.json
 echo "Wiremock started"
