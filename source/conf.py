2d6a5272d1ef613a9e1d5b1f2058396298aa6566
Eric Avdey <eiri@eiri.ca>
2023-05-29 14:43:17 -0300

    refactor(docs): parse docstrings as markdown
    We have markdown in docstrings of our generated modules. This causes
    issues with sphinx autodoc parser.
    
    - Switch to autodoc2 and myst to properly parse markdown.
    - Patch invalid markdown before parsing.
    - Switch to "Read the Docs" theme and normalize TOC for better presentation.
    
    Closes #78

--- source/conf.py
+++ source/conf.py
@@ -18,11 +18,10 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 print(os.path.abspath('..'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Cloudant Python SDK'
-copyright = 'Copyright IBM Corp. 2021'
+copyright = 'Copyright IBM Corp. 2021, 2023'
 author = '@IBM/cloudant-sdks'
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +30,33 @@ author = '@IBM/cloudant-sdks'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'autodoc2',
+    'myst_parser',
+    'sphinx_rtd_theme',
+]
+
+myst_enable_extensions = ['colon_fence', 'fieldlist']
+
+autodoc2_packages = [
+    {
+        'path': '../ibmcloudant',
+        'exclude_files': [
+            'common.py',
+            'version.py',
+            'cloudant_base_service.py',
+            'couchdb_session_get_authenticator_patch.py'
+        ]
+    }
+]
+
+autodoc2_sort_names = True
+
+autodoc2_index_template = None
+
+autodoc2_hidden_objects = ['dunder', 'private', 'inherited']
+
+autodoc2_docstring_parser_regexes = [
+    (r'.*', 'myst'),
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,15 +65,41 @@ extensions = [
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['apidocs/ibmcloudant/ibmcloudant.rst']
 
+# -- Patch generated rst -----------------------------------------------------
+
+file_path = '../ibmcloudant/cloudant_v1.py'
+print(f'[patching] file {file_path}')
+# read source file
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+# patch broken markup
+codeblock = False
+for i in range(len(lines)):
+    # replace class vars to :param: for better rendering
+    if ':attr ' in lines[i]:
+        lines[i] = lines[i].replace(':attr', ':param')
+    # replace note with tip admonition
+    if '### Note' in lines[i]:
+        lines[i] = '        :::{tip}\n'
+        codeblock = True
+    if codeblock and lines[i] == '\n':
+        lines[i] = '        :::\n\n'
+        codeblock = False
+# write back
+with open(file_path, 'w') as file:
+    file.writelines(lines)
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
