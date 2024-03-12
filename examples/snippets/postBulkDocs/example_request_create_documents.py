021bc9562210ad5fc0e58f7d933c8ccdcae53dcd
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2024-03-12 18:49:08 +0000

    docs(generated): update examples for serde changes
    Generated SDK source code using:
    - Generator version 3.86.0
    - Specification version 1.0.0-dev0.1.6
    - Automation (cloudant-sdks) version 67e7ce7
    
    Signed-off-by: cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>

--- examples/snippets/postBulkDocs/example_request_create_documents.py
+++ examples/snippets/postBulkDocs/example_request_create_documents.py
@@ -4,7 +4,7 @@ from ibmcloudant.cloudant_v1 import Document, CloudantV1, BulkDocs
 service = CloudantV1.new_instance()
 
 event_doc_1 = Document(
-  id="0007241142412418284",
+  _id="0007241142412418284",
   type="event",
   userid="abc123",
   eventType="addedToBasket",
@@ -12,7 +12,7 @@ event_doc_1 = Document(
   date="2019-01-28T10:44:22.000Z"
 )
 event_doc_2 = Document(
-  id="0007241142412418285",
+  _id="0007241142412418285",
   type="event",
   userid="abc234",
   eventType="addedToBasket",
