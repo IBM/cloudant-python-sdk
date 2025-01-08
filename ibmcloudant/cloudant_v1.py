4ee850cf974118ae38f138b0f6dd542c7d6f26a3
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2025-01-08 12:21:08 +0000

    chore(generated): 2025 updates
    Generated SDK source code using:
    - Generator version 3.98.0
    - Specification version 1.0.0-dev0.1.23
    - Automation (cloudant-sdks) version c1e4608

--- ibmcloudant/cloudant_v1.py
+++ ibmcloudant/cloudant_v1.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# (C) Copyright IBM Corp. 2024.
+# (C) Copyright IBM Corp. 2025.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
