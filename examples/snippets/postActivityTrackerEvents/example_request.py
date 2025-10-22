f861d993e90ae6c5520f150191319dd67504acdf
cloudant-sdks-automation <71659186+cloudant-sdks-automation@users.noreply.github.com>
2025-10-22 09:09:19 +0000

    PR feedback(generated): use enum values instead of plain strings
    Generated SDK source code using:
    - Generator version 3.107.1
    - Specification version 1.0.0-dev0.1.29
    - Automation (cloudant-sdks) version 52aab7c

--- examples/snippets/postActivityTrackerEvents/example_request.py
+++ examples/snippets/postActivityTrackerEvents/example_request.py
@@ -1,10 +1,13 @@
 # section: code
-from ibmcloudant.cloudant_v1 import CloudantV1
+from ibmcloudant.cloudant_v1 import CloudantV1, ActivityTrackerEvents
 
 service = CloudantV1.new_instance()
 
 response = service.post_activity_tracker_events(
-  types=['management']
+  types=[
+    ActivityTrackerEvents.TypesEnum.MANAGEMENT,
+    ActivityTrackerEvents.TypesEnum.DATA
+  ]
 ).get_result()
 
 print(response)
